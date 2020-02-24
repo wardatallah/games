5f8d07ce0099c1c43e1bab72c1d269690903b074
Ward Atallah <wardatallah6@hotmail.com>
2020-02-23 23:23:50 -0500

    Updates

---
+++ precache-manifest.91f6c500b5e22a9c92550195188930f2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fefc928ac62e29b033ab6bca76758f",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "3b8624dd0472a2159049",
+    "url": "/games/static/css/2.5af0b225.chunk.css"
+  },
+  {
+    "revision": "97e6ae1c92d34ce61f76",
+    "url": "/games/static/css/main.feca1d81.chunk.css"
+  },
+  {
+    "revision": "3b8624dd0472a2159049",
+    "url": "/games/static/js/2.be310599.chunk.js"
+  },
+  {
+    "revision": "97e6ae1c92d34ce61f76",
+    "url": "/games/static/js/main.c8fb0f4b.chunk.js"
+  },
+  {
+    "revision": "c22cdf1719c54fbcf5a5",
+    "url": "/games/static/js/runtime~main.42833f76.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/games/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/games/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/games/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/games/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/games/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/games/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/games/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/games/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/games/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/games/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
