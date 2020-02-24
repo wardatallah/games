baa1cda8763e7f5caa5fb2b5c9f707736f986ad0
Ward Atallah <wardatallah6@hotmail.com>
2020-02-23 23:58:54 -0500

    Updates

---
+++ precache-manifest.a438c8573e9d828b08c61323094426b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce74869f38ff7350bd8c09f2eab5f4f",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "fe08a40c7cef5332b3e8",
+    "url": "/games/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "fb11355d067512390ff5",
+    "url": "/games/static/js/2.7c156803.chunk.js"
+  },
+  {
+    "revision": "fe08a40c7cef5332b3e8",
+    "url": "/games/static/js/main.892dda42.chunk.js"
+  },
+  {
+    "revision": "c22cdf1719c54fbcf5a5",
+    "url": "/games/static/js/runtime~main.42833f76.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/games/precache-manifest.21ba508bde367a59b0426bd4d972bc7e.js"
+  "/games/precache-manifest.a438c8573e9d828b08c61323094426b9.js"
 );
 
 self.addEventListener('message', (event) => {
